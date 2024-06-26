721938b4d5415bab16d70ca4bcc883ad98726e53
IlyA1536 <i0977187143@gmailcom>
2024-06-26 14:35:44 +0300

    Add CustomUser to admin. Create restaurants startapp.

--- user/models.py
+++ user/models.py
@@ -13,7 +13,6 @@ class CustomUser(AbstractUser):
     last_name = models.CharField(max_length=30)
     email = models.EmailField(blank=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
-    is_admin = models.BooleanField(default=False)
     address = models.CharField(max_length=255, blank=True, null=True)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='N')
     date_of_birth = models.DateField(null=True, blank=True)
