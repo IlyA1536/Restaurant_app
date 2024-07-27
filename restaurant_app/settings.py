ff99ca6dc4809a21aa81ce6aa5e400a2b4734941
Illia <i0977187143@gmail.com>
2024-07-27 14:05:31 +0300

    Update settings for Render.com

--- restaurant_app/settings.py
+++ restaurant_app/settings.py
@@ -21,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0nx9ew%md7s1!94pr(#75ek+q(=get--=2+*nc1nbd=93a7q+_'
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY', 'ei4sgt^ugs@klpyo0x30ccv0wc@jn%u68qbc!dp$30w3w*wk)#'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', True)
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST', '127.0.0.1')]
 
 
 # Application definition
