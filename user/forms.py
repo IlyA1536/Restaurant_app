9d6d0c6347cdb13d0414fa7893f6cc6ab520e67f
IlyA1536 <i0977187143@gmailcom>
2024-08-15 22:01:28 +0300

    Bug fix. Add validation for phone number.

--- user/forms.py
+++ user/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import CustomUser
+import re
 
 class CustomUserForm(forms.ModelForm):
     class Meta:
@@ -12,3 +13,11 @@ class CustomUserForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(CustomUserForm, self).__init__(*args, **kwargs)
         self.fields['avatar'].label = "Change Avatar"
+
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data.get('phone_number')
+        if phone_number:
+            if not re.match(r'^\d{9}$', phone_number):
+                raise forms.ValidationError('Please enter the last 9 digits of your Ukrainian phone number.')
+            return f'+380{phone_number}'
+        return phone_number
